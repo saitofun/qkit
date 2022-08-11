092c774771e5a393fad725e02371a588f7ef7026
sincos <birdyfj@gmail.com>
2022-08-11 08:52:00 +0800

    fix: pager default value

--- kit/sqlx/datatypes/pager.go
+++ kit/sqlx/datatypes/pager.go
@@ -4,7 +4,7 @@ import "github.com/saitofun/qkit/kit/sqlx/builder"
 
 type Pager struct {
 	Size   int64 `name:"size,omitempty"   in:"query" default:"10" validate:"@int64[-1,]"`
-	Offset int64 `name:"offset,omitempty" in:"query" default:"10" validate:"@int64[0,]"`
+	Offset int64 `name:"offset,omitempty" in:"query" default:"0"  validate:"@int64[0,]"`
 }
 
 func (p *Pager) Addition() builder.Addition {
